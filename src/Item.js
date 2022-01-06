2592b27464d03f0eb58d7b1906c9aa837ea12f6c
Ikhsan Haikal <47350060+ikhsanhaikal@users.noreply.github.com>
2022-01-06 20:16:07 +0700

    added a Slide component for changing page

--- src/Item.js
+++ src/Item.js
@@ -3,30 +3,39 @@ import {
   Heading,
   HStack,
   Text,
-  VStack,
-  Flex,
-  useMediaQuery,
+  Slide,
+  useDisclosure,
 } from "@chakra-ui/react";
-
+import LecturerView from "./LecturerView";
 const Item = ({ doc }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <HStack
-      bg={"white"}
-      width={"full"}
-      alignItems={"start"}
-      borderWidth={1}
-      p={3}
-      my={1}
-      _hover={{ cursor: "pointer" }}
-    >
-      <Box w={"100px"} h={"100px"} bg={"#f2f2f2"}></Box>
-      <Box>
-        <Heading fontSize={["lg", "xl"]} textAlign={"start"}>
-          apple
-        </Heading>
-        <Text>expertise {"\u2022"} computer networking</Text>
-      </Box>
-    </HStack>
+    <>
+      <HStack
+        bg={"white"}
+        width={"full"}
+        alignItems={"start"}
+        borderWidth={1}
+        p={3}
+        my={1}
+        _hover={{ cursor: "pointer" }}
+        onClick={() => {
+          console.log("Oucch");
+          onOpen();
+        }}
+      >
+        <Box w={"100px"} h={"100px"} bg={"gray.200"}></Box>
+        <Box>
+          <Heading fontSize={["lg", "xl"]} textAlign={"start"}>
+            apple
+          </Heading>
+          <Text>expertise {"\u2022"} computer networking</Text>
+        </Box>
+      </HStack>
+      <Slide direction="left" in={isOpen} style={{ zIndex: 10 }}>
+        <LecturerView onClose={onClose} />
+      </Slide>
+    </>
   );
 };
 
